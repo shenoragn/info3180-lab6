1e973fb103df8fac2b9fac3465d6e1d4652a1975
shenoragn <shenoranglin95@gmail.com>
2022-04-04 23:52:20 -0500

    final commit to VueJs project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script>
-import NewsList from '@/components/NewsList.vue'
 export default {
     data() {
         return {
             message: "Hello World! Welcome to VueJS"
         };
     },
-    components: { NewsList }
 }
 </script>
 
@@ -15,7 +13,6 @@ export default {
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
-        <NewsList></NewsList>
       </div>
     </div>
 </template>
---
+++ src/views/NewsView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="news container">
+        <h2>News</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+    return {};
+    },
+};
+</script>
+
+<style></style>
